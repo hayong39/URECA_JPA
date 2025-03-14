81b62715c4ce52f1d68ad71de2e723c99550ac6b
hayong39 <yia0524_hy@naver.com>
2025-03-14 16:52:34 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/kr/co/ureca/Jpa250314Application.java
@@ -0,0 +1,13 @@
+package kr.co.ureca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa250314Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpa250314Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/ureca/Jpa250314ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ureca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa250314ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
